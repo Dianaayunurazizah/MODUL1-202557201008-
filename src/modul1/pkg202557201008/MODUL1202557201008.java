636f9c5a00db07eafcff3696333f99ed4605ed8d
Dianaayunurazizah <dianaayunurazizah646@gmail.com>
2026-04-20 11:44:40 +0700

    CODING AWAL

---
+++ src/modul1/pkg202557201008/MODUL1202557201008.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul1.pkg202557201008;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Acer
+ */
+public class MODUL1202557201008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int [][] data = {{4,6,4,2,8,4,2,10},{4,6,4,2,8,4,2,10}};
+    for ( int a=0; a<data.length; a++){
+        for ( int b=0; b<data[a].length; b++){
+            System.out.print(data[a][b]+" ");
+        }
+        System.out.println("\n");
+    }
+    }
+    
+}
